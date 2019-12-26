2fd951922650e13d379cb77da305f230d2e069a7
Michele Da Rin Fioretto <michele@daudr.me>
2019-12-26 22:59:27 +0100

    fix(articlecard): fix ArticleCard tags' link

--- src/components/article-card/article-card.js
+++ src/components/article-card/article-card.js
@@ -11,84 +11,84 @@ import { rhythm } from '../../utils/typography'
 import kebabCase from 'lodash/kebabCase'
 
 const useStyles = makeStyles({
-	card: {
-		minWidth: 275,
-		marginBottom: rhythm(1 / 2),
-	},
-	title: {
-		fontSize: 14,
-	},
-	pos: {
-		marginBottom: 12,
-	},
-	chip: {
-		margin: `2px`,
-	},
-	media: {
-		height: 280,
-	},
+  card: {
+    minWidth: 275,
+    marginBottom: rhythm(1 / 2),
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  chip: {
+    margin: `2px`,
+  },
+  media: {
+    height: 280,
+  },
 })
 
 export const ArticleCard = ({ node, isIndex = false, isIT = false }) => {
-	const title = node.frontmatter.title || node.fields.slug
-	const tags = node.frontmatter.tags || []
-	const classes = useStyles()
+  const title = node.frontmatter.title || node.fields.slug
+  const tags = node.frontmatter.tags || []
+  const classes = useStyles()
 
-	return (
-		<Link to={node.fields.slug} className='article-card__link'>
-			<Card className={classes.card}>
-				<CardMedia
-					className={classes.media}
-					image={node.frontmatter.cover_image}
-					title={title}
-				/>
-				<CardContent>
-					<Typography
-						className={classes.title}
-						color='textSecondary'
-						gutterBottom
-					>
-						{tags.slice(0, 3).map(tag => {
-							return (
-								<Link
-									key={tag}
-									to={`/tags/${isIT ? `it/` : ``}${kebabCase(tag)}`}
-									style={{ boxShadow: `none` }}
-								>
-									<Chip
-										size='small'
-										label={`#${tag}`}
-										className={classes.chip}
-										style={{
-											fontFamily: `'Anton', sans-serif`,
-											fontWeight: `bold`,
-											textTransform: `uppercase`,
-										}}
-										onClick={() => {}}
-									></Chip>
-								</Link>
-							)
-						})}
-					</Typography>
-					<Typography variant='h5' component='h2' data-cy='post-title'>
-						{title}
-					</Typography>
-					<Typography
-						className={classes.pos}
-						variant='subtitle2'
-						component='p'
-						color='textSecondary'
-					>
-						{node.frontmatter.date}
-					</Typography>
+  return (
+    <Link to={node.fields.slug} className='article-card__link'>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={node.frontmatter.cover_image}
+          title={title}
+        />
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color='textSecondary'
+            gutterBottom
+          >
+            {tags.slice(0, 3).map(tag => {
+              return (
+                <Link
+                  key={tag}
+                  to={`${isIT ? `/it` : ``}/tags/${kebabCase(tag)}`}
+                  style={{ boxShadow: `none` }}
+                >
+                  <Chip
+                    size='small'
+                    label={`#${tag}`}
+                    className={classes.chip}
+                    style={{
+                      fontFamily: `'Anton', sans-serif`,
+                      fontWeight: `bold`,
+                      textTransform: `uppercase`,
+                    }}
+                    onClick={() => {}}
+                  ></Chip>
+                </Link>
+              )
+            })}
+          </Typography>
+          <Typography variant='h5' component='h2' data-cy='post-title'>
+            {title}
+          </Typography>
+          <Typography
+            className={classes.pos}
+            variant='subtitle2'
+            component='p'
+            color='textSecondary'
+          >
+            {node.frontmatter.date}
+          </Typography>
 
-					<Typography variant='body2' component='p'>
-						{node.frontmatter.description || node.excerpt}
-					</Typography>
-				</CardContent>
-			</Card>
-		</Link>
-	)
+          <Typography variant='body2' component='p'>
+            {node.frontmatter.description || node.excerpt}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
+  )
 }
 
 export default ArticleCard
